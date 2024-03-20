7832b26d858ecac35ecd083015464f4706a3ab17
Zoran Bovin <zoran.bovin@student.ucll.be>
2024-03-20 11:11:52 +0100

    Add project files.

---
+++ CS2itemViewer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CS2itemViewer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
