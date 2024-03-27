7823bbf21090f1fe3a202718837524122d925299
Zoran Bovin <zoran.bovin@student.ucll.be>
2024-03-27 11:44:16 +0100

    UI changes + portrait mode locked

--- CS2itemViewer/Platforms/Android/MainActivity.cs
+++ CS2itemViewer/Platforms/Android/MainActivity.cs
@@ -7,5 +7,13 @@ namespace CS2itemViewer
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Add this line to lock orientation to portrait
+            RequestedOrientation = Android.Content.PM.ScreenOrientation.Portrait;
+            // Load the main application
+            
+        }
     }
 }
