bc4cfbc3db1d297f0bef36cc8e30f2964a1c4b49
Zoran Bovin <zoran.bovin@student.ucll.be>
2024-04-18 14:46:21 +0200

    ok

---
+++ CS2itemViewer/ViewModel/SkinViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2itemViewer.ViewModel
+{
+    public partial class SkinViewModel : BaseViewModel
+    {
+
+    }
+}
