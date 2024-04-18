7d9d5b6a2fd130761ecbcf7bcc243ec051a1acbb
Zoran Bovin <zoran.bovin@student.ucll.be>
2024-04-18 15:59:54 +0200

    alot of stuff

---
+++ CS2itemViewer/DetailsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CS2itemViewer;
+
+public partial class DetailsPage : ContentPage
+{
+	public DetailsPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CS2itemViewer/ViewModel/SkinDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CS2itemViewer.Model;
+using CS2itemViewer.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyApp.ViewModel
+{
+    [QueryProperty(nameof(Skin), "Skin")]
+    public partial class MonkeyDetailsViewModel : BaseViewModel  // <<<<<<<<<<<<<<<<<<<<<< LOOK HERE AGAIN
+    {
+        IMap map;
+        public MonkeyDetailsViewModel(IMap map)
+        {
+            this.map = map;
+
+
+        }
+
+        [ObservableProperty]
+        Skin? skin;
+
+    }
+}
