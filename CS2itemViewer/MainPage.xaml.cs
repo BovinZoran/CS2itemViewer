dd9d16a8ec34b039c263f3ab4969fdf3d615b3fb
Ragnar55 <126241335+Ragnar55@users.noreply.github.com>
2024-06-10 10:51:39 +0200

    white space

--- CS2itemViewer/MainPage.xaml.cs
+++ CS2itemViewer/MainPage.xaml.cs
@@ -6,20 +6,13 @@ namespace CS2itemViewer
 {
     public partial class MainPage : ContentPage
     {
-       
-
         public MainPage(/*SkinViewModel viewModel*/)
         {
             InitializeComponent();
             //BindingContext = viewModel;
             var viewModel = new SkinViewModel(new SkinService(), Connectivity.Current, Navigation);
             BindingContext = viewModel;
-
         }
-
-
-       
-
         private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
         {
             var viewModel = BindingContext as SkinViewModel;
@@ -28,13 +21,6 @@ namespace CS2itemViewer
                 viewModel.FilterSkinsCommand.Execute(null);
             }
         }
-
-        
-
-        
-
-
-
     }
 
 }
