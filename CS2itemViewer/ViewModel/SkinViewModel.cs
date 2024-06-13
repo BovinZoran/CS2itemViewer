e4e27b07acfb0dee15e8c037945bba36ed795078
Zoran Bovin <zoran.bovin@student.ucll.be>
2024-06-13 21:48:44 +0200

    opmaak code

--- CS2itemViewer/ViewModel/SkinViewModel.cs
+++ CS2itemViewer/ViewModel/SkinViewModel.cs
@@ -21,17 +21,6 @@ namespace CS2itemViewer.ViewModel
         private readonly ISkinService _skinService;
         private readonly IConnectivity _connectivity;
         private List<Skin> allSkins;
-        private bool _isSortByPriceAscending = false;
-        private bool _isSortByRarityAscending= false;  
-
-        public ICommand SortByPriceCommand => new Command(SortByPrice);
-        public ICommand SortByRarityCommand => new Command(SortByRarity);
-
-       
-
-        public ICommand OpenLinkCommand { get; }
-        public Command OnSearchedCommand { get; }
-        public ICommand LoadLoginCommand { get; }
 
         public SkinViewModel(ISkinService skinService, IConnectivity connectivity)
         {
@@ -40,65 +29,61 @@ namespace CS2itemViewer.ViewModel
             _connectivity = connectivity;
             allSkins = new List<Skin>();
 
-            
+            SteamLoginIDText = "76561198268749335"; // Zoran's Steam Id
 
             OpenLinkCommand = new RelayCommand<string>(OpenLink);
 
-            SteamLoginIDText = "76561198268749335"; // remove later
-
             OnSearchedCommand = new Command(OnSearched);
 
-            // Initialize LoadLoginCommand
             LoadLoginCommand = new RelayCommand(UpdateSteamID);
 
             // Call the GetSkinsCommand command when the ViewModel is constructed
             GetSkinsCommand.Execute(null);
         }
 
-        public ICommand SortByPriceAscendingDescendingCommand => new Command(SortByPriceAscendingDescending);
-        private string _priceAscendingDescending = "Price Asc";
-        private void SortByPriceAscendingDescending()
+        //-------Open the filtering menu--------
+        public ICommand ShowFilterMenuCommand => new Command(ShowFilterMenu);
+        private void ShowFilterMenu()
         {
-           
-            IsSortByPriceAscending = !IsSortByPriceAscending;
-            PriceAscendingDescending = IsSortByPriceAscending ? "Price Desc" : "Price Asc";
+            IsFilterMenuVisible = !IsFilterMenuVisible;
 
-        }
-        public string PriceAscendingDescending
-        {
-            get => _priceAscendingDescending;
-            set => SetProperty(ref _priceAscendingDescending, value);
-        }
-
-
-
-        public ICommand SortByRarityAscendingDescendingCommand => new Command(SortByRarityAscendingDescending);
-        private string _rarityAscendingDescending = "Rarity Asc";
-        private void SortByRarityAscendingDescending()
-        {
-     
-                IsSortByRarityAscending = !IsSortByRarityAscending;
-                RarityAscendingDescending = IsSortByRarityAscending ? "Rarity Desc" : "Rarity Asc";   
-
-        }
-        public string RarityAscendingDescending
-        {
-            get => _rarityAscendingDescending;
-            set => SetProperty(ref _rarityAscendingDescending, value);
-        }
-
-
-        [RelayCommand]
-        async Task GoToDetails(Skin selectedSkin)
-        {
-            if (selectedSkin != null)
+            if (IsLoginMenuVisible)
             {
-                var skinJson = JsonConvert.SerializeObject(selectedSkin);
-                var encodedSkinJson = HttpUtility.UrlEncode(skinJson); // Encode the JSON string
-                await Shell.Current.GoToAsync($"DetailsPage?skin={encodedSkinJson}");
+                IsLoginMenuVisible = !IsLoginMenuVisible;
+            }
+
+        }
+
+        private bool _isFilterMenuVisible;
+        public bool IsFilterMenuVisible
+        {
+            get => _isFilterMenuVisible;
+            set => SetProperty(ref _isFilterMenuVisible, value);
+        }
+
+        //-------Open the login menu--------
+        public ICommand ShowLoginMenuCommand => new Command(ShowLoginMenu);
+        private void ShowLoginMenu()
+        {
+            IsLoginMenuVisible = !IsLoginMenuVisible;
+
+            if (IsFilterMenuVisible)
+            {
+                IsFilterMenuVisible = !IsFilterMenuVisible;
             }
         }
 
+        private bool _isLoginMenuVisible;
+        public bool IsLoginMenuVisible
+        {
+            get => _isLoginMenuVisible;
+            set => SetProperty(ref _isLoginMenuVisible, value);
+        }
+
+        //-------Load in Steam invertory with sID--------
+        [ObservableProperty]
+        private string steamLoginIDText;
+        public ICommand LoadLoginCommand { get; }
         private void UpdateSteamID()
         {
             IsLoginMenuVisible = !IsLoginMenuVisible;
@@ -106,6 +91,8 @@ namespace CS2itemViewer.ViewModel
             GetSkinsCommand.Execute(null);
         }
 
+        //-------Open link to Steam ID page--------
+        public ICommand OpenLinkCommand { get; }
         private void OpenLink(string url)
         {
             try
@@ -119,22 +106,118 @@ namespace CS2itemViewer.ViewModel
             }
         }
 
-        [ObservableProperty]
-        bool isRefreshing;
-
+        //-------checking item names for seached item--------
         [ObservableProperty]
         string searchText;
 
-        [ObservableProperty]
-        private string steamLoginIDText;
+        public Command OnSearchedCommand { get; }
 
+        [RelayCommand]
+        private void OnSearched()
+        {
+            FilterSkins();
+        }
 
+        //-------Sorting by Price--------
+        public ICommand SortByPriceAscendingDescendingCommand => new Command(SortByPriceAscendingDescending);
+        private string _priceAscendingDescending = "Price Asc";
+        private void SortByPriceAscendingDescending()
+        {
+
+            IsSortByPriceAscending = !IsSortByPriceAscending;
+            PriceAscendingDescending = IsSortByPriceAscending ? "Price Desc" : "Price Asc";
+
+        }
+        public string PriceAscendingDescending
+        {
+            get => _priceAscendingDescending;
+            set => SetProperty(ref _priceAscendingDescending, value);
+        }
+
+        private bool _isSortByPriceAscending = false;
+        public bool IsSortByPriceAscending
+        {
+            get => _isSortByPriceAscending;
+            set
+            {
+                SetProperty(ref _isSortByPriceAscending, value);
+                FilterSkins();
+            }
+        }
+
+        private bool _SortByPriceChecked = false;
+        public bool SortByPriceChecked
+        {
+            get => _SortByPriceChecked;
+            set
+            {
+                SetProperty(ref _SortByPriceChecked, value);
+                FilterSkins();
+            }
+        }
+
+        //-------Sorting by Rarity--------
+        public ICommand SortByRarityAscendingDescendingCommand => new Command(SortByRarityAscendingDescending);
+        private string _rarityAscendingDescending = "Rarity Asc";
+        private void SortByRarityAscendingDescending()
+        {
+
+            IsSortByRarityAscending = !IsSortByRarityAscending;
+            RarityAscendingDescending = IsSortByRarityAscending ? "Rarity Desc" : "Rarity Asc";
+
+        }
+        public string RarityAscendingDescending
+        {
+            get => _rarityAscendingDescending;
+            set => SetProperty(ref _rarityAscendingDescending, value);
+        }
+
+        private bool _isSortByRarityAscending = false;
+        public bool IsSortByRarityAscending
+        {
+            get => _isSortByRarityAscending;
+            set
+            {
+                SetProperty(ref _isSortByRarityAscending, value);
+                FilterSkins();
+            }
+        }
+
+        private bool _SortByRarityChecked = false;
+
+        public bool SortByRarityChecked
+        {
+            get => _SortByRarityChecked;
+            set
+            {
+                SetProperty(ref _SortByRarityChecked, value);
+                FilterSkins();
+            }
+        }
+
+        //-------Go to detailsPage with skin info--------
+        [RelayCommand]
+        async Task GoToDetails(Skin selectedSkin)
+        {
+            if (selectedSkin != null)
+            {
+                var skinJson = JsonConvert.SerializeObject(selectedSkin);
+                var encodedSkinJson = HttpUtility.UrlEncode(skinJson); // Encode the JSON string
+                await Shell.Current.GoToAsync($"DetailsPage?skin={encodedSkinJson}");
+            }
+        }
+
+        //-------Setting price to 0.00 accurate--------
         private double totalPriceLatestSell;
         public double TotalPriceLatestSell
         {
             get => totalPriceLatestSell;
             set => SetProperty(ref totalPriceLatestSell, Math.Round(value, 2));
         }
+
+        //-------Gettiing skins from API + error messages if unable--------
+        [ObservableProperty]
+        bool isRefreshing;
 
         [RelayCommand]
         async Task GetSkinsAsync()
@@ -172,6 +255,22 @@ namespace CS2itemViewer.ViewModel
             }
         }
 
+        //-------Filtering--------
+        private int GetRarityOrder(string color)
+        {
+            return color switch
+            {
+                "#b0c3d9" => 1, // Consumer Grade
+                "#5e98d9" => 2, // Industrial Grade
+                "#4b69ff" => 3, // High Grade
+                "#8847ff" => 4, // Restricted
+                "#d32ce6" => 5, // Classified
+                "#eb4b4b" => 6, // Covert
+                "#e4ae39" => 7, // Contraband
+                _ => 8 // Any other color
+            };
+        }
+
         [RelayCommand]
         void FilterSkins()
         {
@@ -179,19 +278,18 @@ namespace CS2itemViewer.ViewModel
                 ? allSkins.Where(IsSkinVisible).ToList()
                 : allSkins.Where(skin => skin.MarketName.Contains(SearchText, StringComparison.OrdinalIgnoreCase) && IsSkinVisible(skin)).ToList();
 
-            if(SortByPriceChecked)
+            if (SortByPriceChecked)
             {
-               // Sort by price
-            if (IsSortByPriceAscending)
-            {
-                filteredSkins = filteredSkins.OrderBy(skin => skin.PriceLatestSell).ToList();
+                // Sort by price if toggled
+                if (IsSortByPriceAscending)
+                {
+                    filteredSkins = filteredSkins.OrderBy(skin => skin.PriceLatestSell).ToList();
+                }
+                else
+                {
+                    filteredSkins = filteredSkins.OrderByDescending(skin => skin.PriceLatestSell).ToList();
+                }
             }
-            else
-            {
-                filteredSkins = filteredSkins.OrderByDescending(skin => skin.PriceLatestSell).ToList();
-            }
-            }
-            
 
             if (SortByRarityChecked)
             {
@@ -206,21 +304,33 @@ namespace CS2itemViewer.ViewModel
                 }
             }
 
-               Skins.Clear();
-               foreach (var skin in filteredSkins)
-               {
-                    Skins.Add(skin);
-               }
-
-            
-
-            
+            Skins.Clear();
+            foreach (var skin in filteredSkins)
+            {
+                Skins.Add(skin);
+            }
 
             // Calculate the total price of visible skins and round to 2 decimal places
             TotalPriceLatestSell = Skins.Sum(skin => skin.PriceLatestSell);
         }
 
-        // Color filter properties
+        //-------checking checkboxes to filter rarity--------
+        bool IsSkinVisible(Skin skin)
+        {
+            if ((IsConsumerGradeChecked && skin.Color == "#b0c3d9") ||
+                (IsIndustrialGradeChecked && skin.Color == "#5e98d9") ||
+                (IsHighGradeChecked && skin.Color == "#4b69ff") ||
+                (IsRestrictedChecked && skin.Color == "#8847ff") ||
+                (IsClassifiedChecked && skin.Color == "#d32ce6") ||
+                (IsCovertChecked && skin.Color == "#eb4b4b") ||
+                (IsContrabandChecked && skin.Color == "#e4ae39"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //-------Rarity checkbox properties--------
         private bool _isConsumerGradeChecked = true;
         private bool _isIndustrialGradeChecked = true;
         private bool _isHighGradeChecked = true;
@@ -228,9 +338,6 @@ namespace CS2itemViewer.ViewModel
         private bool _isClassifiedChecked = true;
         private bool _isCovertChecked = true;
         private bool _isContrabandChecked = true;
-
-        private bool _SortByPriceChecked = false;
-        private bool _SortByRarityChecked = false;
 
         public bool IsConsumerGradeChecked
         {
@@ -300,139 +407,6 @@ namespace CS2itemViewer.ViewModel
                 SetProperty(ref _isContrabandChecked, value);
                 FilterSkins();
             }
-        }
-
-        public bool SortByPriceChecked
-        {
-            get => _SortByPriceChecked;
-            set
-            {
-                SetProperty(ref _SortByPriceChecked, value);
-                //SortByPriceCommand.Execute(this);
-                FilterSkins();
-            }
-        }
-
-        public bool SortByRarityChecked
-        {
-            get => _SortByRarityChecked;
-            set
-            {
-                SetProperty(ref _SortByRarityChecked, value);
-                //SortByRarityCommand.Execute(this);
-                FilterSkins();
-            }
-        }
-
-
-
-
-
-
-        private bool _isFilterMenuVisible;
-        public bool IsFilterMenuVisible
-        {
-            get => _isFilterMenuVisible;
-            set => SetProperty(ref _isFilterMenuVisible, value);
-        }
-
-        public ICommand ShowFilterMenuCommand => new Command(ShowFilterMenu);
-
-        private void ShowFilterMenu()
-        {
-            IsFilterMenuVisible = !IsFilterMenuVisible;
-            
-            if(IsLoginMenuVisible) 
-            {
-            IsLoginMenuVisible = !IsLoginMenuVisible;
-            }
-            
-        }
-
-        private bool _isLoginMenuVisible;
-        public bool IsLoginMenuVisible
-        {
-            get => _isLoginMenuVisible;
-            set => SetProperty(ref _isLoginMenuVisible, value);
-        }
-
-        public ICommand ShowLoginMenuCommand => new Command(ShowLoginMenu);
-        private void ShowLoginMenu()
-        {
-            IsLoginMenuVisible = !IsLoginMenuVisible;
-
-            if (IsFilterMenuVisible)
-            {
-                IsFilterMenuVisible = !IsFilterMenuVisible;
-            }
-        }
-
-        bool IsSkinVisible(Skin skin)
-        {
-            if ((IsConsumerGradeChecked && skin.Color == "#b0c3d9") ||
-                (IsIndustrialGradeChecked && skin.Color == "#5e98d9") ||
-                (IsHighGradeChecked && skin.Color == "#4b69ff") ||
-                (IsRestrictedChecked && skin.Color == "#8847ff") ||
-                (IsClassifiedChecked && skin.Color == "#d32ce6") ||
-                (IsCovertChecked && skin.Color == "#eb4b4b") ||
-                (IsContrabandChecked && skin.Color == "#e4ae39"))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        
-
-        public bool IsSortByPriceAscending
-        {
-            get => _isSortByPriceAscending;
-            set
-            {
-                SetProperty(ref _isSortByPriceAscending, value);
-                FilterSkins();
-            }
-        }
-
-        public bool IsSortByRarityAscending
-        {
-            get => _isSortByRarityAscending;
-            set
-            {
-                SetProperty(ref _isSortByRarityAscending, value);
-                FilterSkins();
-            }
-        }
-
-        [RelayCommand]
-        private void OnSearched()
-        {
-            FilterSkins();
-        }
-
-        private void SortByPrice()
-        {
-            IsSortByPriceAscending = !IsSortByPriceAscending;
-        }
-
-        private void SortByRarity()
-        {
-            IsSortByRarityAscending = !IsSortByRarityAscending;
-        }
-
-        private int GetRarityOrder(string color)
-        {
-            return color switch
-            {
-                "#b0c3d9" => 1, // Consumer Grade
-                "#5e98d9" => 2, // Industrial Grade
-                "#4b69ff" => 3, // High Grade
-                "#8847ff" => 4, // Restricted
-                "#d32ce6" => 5, // Classified
-                "#eb4b4b" => 6, // Covert
-                "#e4ae39" => 7, // Contraband
-                _ => 8 // Any other color
-            };
         }
     }
 }
